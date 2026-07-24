78a780e9ec77238cf595fb926624277bbee267ff
Merin Elizabeth Edgar <elizamerin10@gmail.com>
2026-07-24 04:03:02 +0000

    Add about page

---
+++ app/about/page.js
@@ -0,0 +1,10 @@
+export default function AboutPage() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>
+       Hi,this is Merin Elizabeth Edgar from S5 CSE D.
+      </p>
+    </div>
+  );
+}
